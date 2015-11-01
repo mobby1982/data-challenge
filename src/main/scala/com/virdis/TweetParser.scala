9fce6d53e94ae130645272af5e4d685abc04e635
sandeep <you@example.com>
2015-11-01 11:00:59 -0800

    tweet parser

---
+++ src/main/scala/com/virdis/TweetParser.scala
@@ -0,0 +1,23 @@
+package com.virdis
+
+/**
+ * Created by sandeep on 11/1/15.
+ */
+
+import org.joda.time.DateTime
+import play.api.libs.json._
+
+case class SecondFeature(hashtags: Seq[String], created_at: DateTime)
+
+trait TweetParser {
+
+  def secondFeatureFormat(input: String): SecondFeature = {
+    val js = Json.parse(input)
+    SecondFeature(
+      (js \ "entities" \ "hashtags" \\ "text").map(_.as[String]) ,
+      (js \ "created_at").as[DateTime]
+    )
+  }
+}
+
+object tweetParser extends TweetParser
