d9f747041257e99cd8dc0c547089da4c45bf3a0b
sandeep <you@example.com>
2015-11-01 11:01:19 -0800

    graph model added

---
+++ src/main/scala/com/virdis/Graph.scala
@@ -0,0 +1,15 @@
+package com.virdis
+
+import scala.collection.immutable.HashMap
+
+/**
+ * Created by sandeep on 11/1/15.
+ */
+
+class Graph {
+
+  var noOfEdges = 0
+
+  var adjacencyStore = HashMap.empty[String, Set[String]]
+
+}
