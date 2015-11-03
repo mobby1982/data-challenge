fab7f4c443ebad0e8f0a4eff8473c39da84baa6c
sandeep <you@example.com>
2015-11-03 09:52:21 -0800

    first feature parsing

--- src/main/scala/com/virdis/TweetParser.scala
+++ src/main/scala/com/virdis/TweetParser.scala
@@ -9,19 +9,35 @@ import play.api.libs.json._
 
 import scala.util.control.NonFatal
 
+case class FirstFeature(text: String, created_at: DateTime)
 case class SecondFeature(hashtags: Seq[String], created_at: DateTime)
 
 trait TweetParser {
 
   /*
-      We only care about tweet data in right format, hence after catching Exception we do nothing
+      We only care about tweet data in correct format,
+      if the data is in incorrect format,
+      after catching Parsing Exception we do nothing
    */
   def secondFeatureFormat(input: String): Option[SecondFeature] = {
     try {
       val js = Json.parse(input)
-
       Option(SecondFeature(
         (js \ "entities" \ "hashtags" \\ "text").map(_.as[String] toLowerCase),
+        (js \ "created_at").as[DateTime]
+      ))
+    } catch {
+      case NonFatal(e)  => {
+        None
+      }
+    }
+  }
+
+  def firstFeatureFormat(input: String): Option[FirstFeature] = {
+    try {
+      val js = Json.parse(input)
+      Option(FirstFeature(
+        (js \ "text").as[String],
         (js \ "created_at").as[DateTime]
       ))
     } catch {
