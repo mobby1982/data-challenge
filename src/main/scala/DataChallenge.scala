9ac1f8c4793e5af04389d19db1570c8e0eede826
sandeep <you@example.com>
2015-11-02 17:45:45 -0800

    different conf configs added

--- src/main/scala/DataChallenge.scala
+++ src/main/scala/DataChallenge.scala
@@ -1,14 +1,24 @@
 package com.virdis.datachallenge
 
+import com.typesafe.config.{ConfigFactory, Config}
 import com.virdis.TwitterHashTagGraph
 
 
 object DataChallenge {
   def main(args: Array[String]) {
+    if (args.isEmpty) {
+      start(ConfigFactory.load("application.conf"))
+    } else if (args(0).equals("test")) {
+      start(ConfigFactory.load("test.conf"))
+    } else if (args(0).equals("prod")) {
+      start(ConfigFactory.load("application.conf"))
+    }
+
+  }
+
+  def start(config: Config) = {
     object twitterGraph extends TwitterHashTagGraph
-    val start = System.currentTimeMillis()
-    twitterGraph.run
-    println("RunTime  "+ (System.currentTimeMillis() - start))
+    twitterGraph.run(config)
   }
 }
 
