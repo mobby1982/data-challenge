43ea8ba126bbc342d5d615cc7e5e4b504a02bd02
sandeep <you@example.com>
2015-11-04 11:38:13 -0800

    handle unicode, empty and duplicate hashtags. tests added

--- src/test/scala/com/virdis/TweetParserSpec.scala
+++ src/test/scala/com/virdis/TweetParserSpec.scala
@@ -71,6 +71,67 @@ class TweetParserSpec extends MySpec {
           |    "timestamp_ms": "1446218985230"
           |}
         """.stripMargin
+
+      val tweetWithUnicode =
+        """
+          |{
+          |    "created_at": "Thu Oct 29 17:51:50 +0000 2015",
+          |    "id": 660116385083838500,
+          |    "id_str": "660116385083838464",
+          |    "text": "Have you got your #pumpkin ready for #Halloween Have Fun! #HappyHalloween #happyfriday https://t.co/UIo2LeYPoZ https://t.co/vamv9XSsH4",
+          |    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+          |    "truncated": false,
+          |    "entities": {
+          |        "hashtags": [
+          |            {
+          |                "text": "HappyHa\u00e3lloween",
+          |                "indices": [
+          |                    58,
+          |                    73
+          |                ]
+          |            },
+          |            {
+          |                "text": "happyfriday\ud83d\udc94",
+          |                "indices": [
+          |                    74,
+          |                    86
+          |                ]
+          |            }
+          |        ]
+          |    },
+          |    "timestamp_ms": "1446218985230"
+          |}
+        """.stripMargin
+
+      val tweetWithDuplicates =
+        """
+          |{
+          |    "created_at": "Thu Oct 29 17:51:50 +0000 2015",
+          |    "id": 660116385083838500,
+          |    "id_str": "660116385083838464",
+          |    "text": "Have you got your #pumpkin ready for #Halloween Have Fun! #HappyHalloween #happyfriday https://t.co/UIo2LeYPoZ https://t.co/vamv9XSsH4",
+          |    "source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+          |    "truncated": false,
+          |    "entities": {
+          |        "hashtags": [ {
+          |                "text": "pumpkin",
+          |                "indices": [
+          |                    18,
+          |                    26
+          |                ]
+          |            },
+          |            {
+          |                "text": "PumpKin",
+          |                "indices": [
+          |                    37,
+          |                    47
+          |                ]
+          |            }]
+          |    },
+          |    "timestamp_ms": "1446218985230"
+          |}
+        """.stripMargin
+
     }
 
 
@@ -94,5 +155,17 @@ class TweetParserSpec extends MySpec {
     val ff = tweetDataParser.firstFeatureFormat(f.tweetWithHTags).get
     ff.text shouldBe a [String]
     ff.created_at shouldBe a [String]
+  }
+
+  it should "clean tweets with unicode characters" in {
+    val f = fixture
+    val sf = tweetDataParser.secondFeatureFormat((f.tweetWithUnicode)).get
+    sf.hashtags.toSet should contain theSameElementsAs Set("happyfriday","HappyHalloween".toLowerCase())
+  }
+
+  it should "remove duplicate hashtags" in {
+    val f = fixture
+    val sf = tweetDataParser.secondFeatureFormat(f.tweetWithDuplicates).get
+    sf.hashtags.toSet should contain theSameElementsAs Set("pumpkin")
   }
 }
