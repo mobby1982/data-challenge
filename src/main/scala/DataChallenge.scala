8a191a89e232e4c4a0ad801d28763bd2efa6b739
sandeep <you@example.com>
2015-11-03 17:56:30 -0800

    unwanted folder

--- src/main/scala/DataChallenge.scala
+++ src/main/scala/DataChallenge.scala
@@ -6,19 +6,16 @@ import com.virdis.{UnicodeTweetCounter, TwitterHashTagGraph}
 
 object DataChallenge {
   def main(args: Array[String]) {
-
+    println("Main Started......")
     if (args.isEmpty) {
       start(ConfigFactory.load("application.conf"))
     } else if (args(0).equals("test")) {
       start(ConfigFactory.load("test.conf"))
-    } else if (args(0).equals("prod")) {
-      start(ConfigFactory.load("application.conf"))
     }
 
   }
 
   def start(config: Config) = {
-    println("Started......")
     object twitterGraph extends TwitterHashTagGraph
     object unicodeCounter extends UnicodeTweetCounter
     unicodeCounter.run(config)
